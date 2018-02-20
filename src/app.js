e82d8b1a251cfdf5f37a429ce7004801f717a484
Sylvain Cousquer <scousquer@talentsoft.com>
2018-02-20 15:49:09 +0100

    forgetted commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBE6VPG0Y2oOs8VugDLMwwi1esYkXFiIWw',
+            authDomain: 'auth-628fc.firebaseapp.com',
+            databaseURL: 'https://auth-628fc.firebaseio.com',
+            projectId: 'auth-628fc',
+            storageBucket: 'auth-628fc.appspot.com',
+            messagingSenderId: '578819605165'
+        });
+    }
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text> An App!</Text>
+                <LoginForm />
             </View>
         );
     }
